0178b3267d854b565faddd12efc0dc11d0bfa971
Remy Bach <remy@bigfish.co.uk>
2012-07-26 11:23:13 +0100

    Remove TODO about eval.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,9 @@
-var PARSE; 
+var PARSE;
 
 jQuery(function($) {
 	var debounce = false;
 
 	$.get('scripts/tests', function(data, textStatus, jqXHR) {
-		// TODO: Do some checking to make sure the data isn't corrupted here.
 		PARSE = eval(data);
 
 		var drag_drop = '<ul>';
@@ -16,24 +15,24 @@ jQuery(function($) {
 		drag_drop+= '</ul>';
 		$('.verbose').before(drag_drop);
 	});
-  
+
 	$('.verbose').keyup(function() {
 		/*if (!debounce) {
 			debounce = new Date().getTime();
 		}
-		
+
 		if (new Date().getTime() - debounce > 200) {
-			
+
 		}*/
 	}).change(function() {
 		var regex = strToRegex(this.value);
 		var regexString = '';
 		var parts = $('.verbose').val().split(',');
-		
+
 		for (i=0; i < regex.length; i++) {
 			regexString+= '<abbr class="part" title="'+parts[i]+'">'+regex[i]+'</abbr>';
 		}
-		
+
 		$('.result').html('Your regex: '+regexString);
 		$('.highlight').html(regexString);
 	});
@@ -41,7 +40,7 @@ jQuery(function($) {
 
 /**
  *	This function receives a string that it parses into a regex Object.
- *	
+ *
  *	@param str The string to parse
  *	@returns regexParts The regex parts representing the regex string passed in. Use by doing `regexParts.join()`
  *	@author: Remy Bach
@@ -49,27 +48,27 @@ jQuery(function($) {
 function strToRegex(str) {
 	var parts = str.split(','),
         regexParts = [];
-  	
+
 	for (i=0; i < parts.length; i++) {
 		var part = parts[i],
 			partSolved = false;
-		
+
 		for (j=0; j < PARSE.length; j++) {
 			var regex = new RegExp('^'+PARSE[j].test+'$');
 			if(window.console&&window.console.log)console.log(PARSE[j]);
 			if ( regex.test(part) ) {
 				regexParts.push(part.replace(regex, PARSE[j].replace));
 				partSolved = true;
-				
+
 				break;
 			}
 		}
-		
+
 		if (!partSolved) {
 			regexParts.push(escapeRegExp(part));
 		}
 	}
-	
+
 	return regexParts;
 }
 
