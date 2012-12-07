223b41ef2e370d772cec1a10ec77d87d0fcd8802
Remy Bach <remy@bach.me.uk>
2012-12-07 00:43:50 +0000

    Major Refactor! Mainly: I've restructured the way the tests are loaded in by just including the script file separately. I've also added comments to the tests file to make multiple people adding tests more ubiquitous.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,69 +1,54 @@
-var PARSE;
-
 jQuery(function($) {
-	var debounce = false;
-
-	$.get('scripts/tests', function(data, textStatus, jqXHR) {
-		PARSE = eval(data);
-
-		var drag_drop = '<ul>';
-		$.each(PARSE, function(i, val) {
-			if (val.name) {
-				drag_drop+= '<li>'+val.name+'</li>';
-			}
-		});
-		drag_drop+= '</ul>';
-		$('.verbose').before(drag_drop);
-	});
-
-	$('.verbose').keyup(function() {
-		/*if (!debounce) {
-			debounce = new Date().getTime();
+	$('.verbose').keyup(function(e) {
+		// If the user presses enter (13), or types a comma (188)
+		if (e.keyCode === 13 || e.keyCode === 188) {
+			parseInput.call(this);
 		}
-
-		if (new Date().getTime() - debounce > 200) {
-
-		}*/
-	}).change(function() {
-		var regex = strToRegex(this.value);
-		var regexString = '';
-		var parts = $('.verbose').val().split(',');
-
-		for (i=0; i < regex.length; i++) {
-			regexString+= '<abbr class="part" title="'+parts[i]+'">'+regex[i]+'</abbr>';
-		}
-
-		$('.result').html('Your regex: '+regexString);
-		$('.highlight').html(regexString);
-	});
+	}).change(parseInput);
 });
 
 /**
  *	This function receives a string that it parses into a regex Object.
- *
- *	@param str The string to parse
- *	@returns regexParts The regex parts representing the regex string passed in. Use by doing `regexParts.join()`
- *	@author: Remy Bach
  */
-function strToRegex(str) {
-	var parts = str.split(','),
-        regexParts = [];
+function parseInput() {
+	var parts = this.value.split(','),
+		regex = strToRegex(parts),
+		regexString = '';
 
-	for (i=0; i < parts.length; i++) {
-		var part = parts[i],
+	for (i=regex.length; i > 0; i--) {
+		regexString+= '<abbr class="part" title="'+parts[regex.length-i]+'">'+regex[regex.length-i]+'</abbr>';
+	}
+
+	$('.result').html('Your regex: '+regexString);
+	$('.highlight').html(regexString);
+}
+
+/**
+ *	This function receives a string that it parses into a regex Object.
+ *
+ *	@param parts An array of the individual bits of the string to parse
+ *	@returns regexParts The regex parts representing the regex string passed in. Use by doing `regexParts.join()`
+ */
+function strToRegex(parts) {
+	var regexParts = [];
+
+	for (var i=parts.length; i > 0; i--) {
+		var part = parts[parts.length - i],
 			partSolved = false;
 
-		for (j=0; j < PARSE.length; j++) {
-			var regex = new RegExp('^'+PARSE[j].test+'$');
-			if(window.console&&window.console.log)console.log(PARSE[j]);
+		for (j=tests.length; j > 0; j--) {
+			var regex = new RegExp('^'+tests[tests.length-j].test+'$');
+			
+			// If this part (i) matches this test (j), we have a winner.
 			if ( regex.test(part) ) {
-				regexParts.push(part.replace(regex, PARSE[j].replace));
+				regexParts.push(part.replace(regex, tests[tests.length-j].replace));
 				partSolved = true;
 
 				break;
 			}
 		}
 
+		// If none of the tests matched, then just give back the same as was input.
 		if (!partSolved) {
 			regexParts.push(escapeRegExp(part));
 		}
---
+++ scripts/tests.js
@@ -0,0 +1,83 @@
+/*
+	This file is the meat & potatoes of this site - as it were.
+	The intention is for this file to be 100% open source which means that
+	adding tests and sending pull requests is absolutely encouraged!
+
+	Having said that, there are a few rules:
+
+	1. Commas at the beginning of the test. This is to make it easier to
+		add new tests as we go along without worry of missing a comma.
+	2. Make sure that any tests reflect the following syntax:
+		 {
+			name:'', // The name of this test. This will be used as a tooltip shown on hovering over the generated regex
+			test:'', // The regex that determines whether this test is a hit
+			replace:'' // The regex that the user was after.
+		 }
+ */
+var tests = [
+	 {
+	 	name:'start of the line',
+	 	test:'(from the )?(start|beginning) of (the )?line',
+	 	replace:'^'
+	 }
+	,{
+		name:'end of the line',
+		test:'(to the )?end of (the )?line',
+		replace:'$'
+	 }
+	,{
+		name:'? numbers',
+		test:'([0-9]+) numbers?',
+		replace:'[0-9]{$1}'
+	 }
+	,{
+		name:'any amount of numbers',
+		test:'any( number of| amount of)? numbers',
+		replace:'[0-9]+'
+	 }
+	,{
+		name:'? characters',
+		test:'([0-9]+) characters?',
+		replace:'[a-zA-Z]{$1}'
+	 }
+	,{
+		name:'any amount of characters',
+		test:'any( number of| amount of)? characters',
+		replace:'[a-zA-Z]+'
+	 }
+	,{
+		name:'any amount of numbers and/or characters',
+		test:'any( number of| amount of)? numbers( &| and| or)? characters',
+		replace:'[a-zA-Z0-9]+'
+	 }
+	,{
+		name:'new line',
+		test:'(new( |\-)?line|enter)',
+		replace:'(\\r)?\\n'
+	 }
+	,{
+		name:'non white space',
+		test:'no(n|t)? white ?space',
+		replace:'\\S'
+	 }
+	,{
+		name:'white space',
+		test:'white ?space',
+		replace:'\\s'
+	 }
+	,{
+		name:'? words',
+		test:'([0-9]+) words',
+		replace:'\\w{$1}'
+	 }
+	,{
+		name:'a word',
+		test:'(a|one|1) word',
+		replace:'\\w'
+	 }
+	,{
+		name:'? letters',
+		test:'([0-9]+) letters',
+		replace:'[a-zA-Z]{$1}'
+	 }
+];
