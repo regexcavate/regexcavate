f00d1a5bb23179344c29ea160c5495b286247e65
Remy Bach <remy@bach.me.uk>
2013-01-22 17:09:50 +0200

    Add new "shortcuts" feature and add/enhance the existing translations.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,6 +8,9 @@ if (/debug=true/.test(location.href)) {
 }
 
 require(["jquery", "translations"], function(jQuery, translations) {
+	var regexParts = [],
+		shortcuts = translations.shortcuts;
+
 	translations = translations.list;
 
 	jQuery(function($) {
@@ -44,10 +47,15 @@ require(["jquery", "translations"], function(jQuery, translations) {
 			regexStringCopy+= regex[regex.length-i];
 		}
 
-		$('.result').html(regexString);
-		$('.result-copy').val(regexStringCopy);
-		$('.example').hide();
-		$('.result-container, .visual').show();
+		if (regexString.length > 0) {
+			$('.result').html(regexString);
+			$('.result-copy').val(regexStringCopy);
+			$('.result-container, .visual').show();
+		} else {
+			$('.result').html('');
+			$('.result-copy').val('');
+			$('.result-container, .visual').hide();
+		}
 	}
 
 	/**
@@ -57,7 +65,8 @@ require(["jquery", "translations"], function(jQuery, translations) {
 	 *	@returns regexParts The regex parts representing the regex string passed in. Use by doing `regexParts.join()`
 	 */
 	function strToRegex(parts) {
-		var regexParts = [];
+		// Reset the array.
+		regexParts.length = 0;
 
 		for (var i=parts.length; i > 0; i--) {
 			var negation = false,
@@ -70,17 +79,7 @@ require(["jquery", "translations"], function(jQuery, translations) {
 				negation = true;
 			}
 
-			for (var j=translations.length; j > 0; j--) {
-				var regex = new RegExp('^'+translations[translations.length-j].in+'$');
-				
-				// If this part (i) matches this test (j), we have a winner.
-				if ( regex.test(part) ) {
-					regexParts.push(part.replace(regex, translations[translations.length-j].out));
-					partSolved = true;
-
-					break;
-				}
-			}
+			partSolved = probe(translations, part);
 
 			// If this part has a regex match AND is a negation AND is a range that doesn't already have a negation in it.
 			if (partSolved && negation && /\[[^\^]/.test(regexParts[regexParts.length-1])) {
@@ -88,13 +87,36 @@ require(["jquery", "translations"], function(jQuery, translations) {
 				regexParts[regexParts.length-1] = regexParts[regexParts.length-1].replace(/\[/, '[^');
 			}
 
-			// If none of the translations matched, then just give back the same as was input.
-			if (!partSolved) {
+			// If none of the translations matched, and this part isn't a shortcut then just give back the same as was input.
+			if (!partSolved && !probe(shortcuts, part)) {
 				regexParts.push(escapeRegExp(part));
 			}
 		}
 
 		return regexParts;
+	}
+
+	/**
+	 *	This function probes the given list for the given part.
+	 *
+	 *	@param list The array of translations/shortcuts we're probing.
+	 *	@param part The particular string we're probing for.
+	 *	@returns partSolved Whether this particular part was found.
+	 */
+	function probe(list, part) {
+		for (var j=list.length; j > 0; j--) {
+			var regex = new RegExp('^'+list[list.length-j].in+'$');
+			
+			// If this part matches this translation, we have a winner.
+			if ( regex.test(part) ) {
+				regexParts.push(part.replace(regex, list[list.length-j].out));
+
+				return true;
+			}
+
+		}
+
+		return false;
 	}
 
 	// This function was found here: http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex
--- scripts/translations.js
+++ scripts/translations.js
@@ -19,190 +19,319 @@
 			regex returned by any of the individual translations below is within a range (ie: '[...]'), then `^`
 			will automatically be prepended inside.
  */
-define({list:[
-	 {
-		name:'anything',
-		in:'anything',
-		out:'.*'
-	 }
-	,{
-		name:'start of the line',
-		in:'(from the )?(start|beginning) of (the )?line',
-		out:'^'
-	 }
-	,{
-		name:'end of the line',
-		in:'(to the )?end of (the )?line',
-		out:'$'
-	 }
-	,{
-		name:'new line',
-		in:'(new( |\-)?line|enter)',
-		out:'(\\r)?\\n'
-	 }
-	,{
-		name:'non white space',
-		in:'no(n|t)? white ?space',
-		out:'\\S'
-	 }
-	,{
-		name:'white space',
-		in:'white ?space',
-		out:'\\s'
-	 }
-	,{
-		name:'a number',
-		in:'(a|a single|one|1) number',
-		out:'[0-9]'
-	 }
-	,{
-		name:'? numbers',
-		in:'([0-9]+) numbers?',
-		out:'[0-9]{$1}'
-	 }
-	,{
-		name:'between ? and ? numbers',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) numbers',
-		out:'[0-9]{$2,$4}'
-	 }
-	,{
-		name:'any amount of numbers',
-		in:'any( number of| amount of)? numbers',
-		out:'[0-9]+'
-	 }
-	,{
-		name:'a letter',
-		in:'(a|a single|one|1)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letter',
-		out:'[a-zA-Z]'
-	 }
-	,{
-		name:'? letters',
-		in:'([0-9]+)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
-		out:'[a-zA-Z]{$1}'
-	 }
-	,{
-		name:'between ? and ? letters',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
-		out:'[a-zA-Z]{$2,$4}'
-	 }
-	,{
-		name:'any amount of letters',
-		in:'any( number of| amount of)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
-		out:'[a-zA-Z]+'
-	 }
-	,{
-		name:'a lowercase letter',
-		in:'(a|a single|one|1) lowercase letter',
-		out:'[a-z]'
-	 }
-	,{
-		name:'? lowercase letters',
-		in:'([0-9]+) lowercase letters',
-		out:'[a-z]{$1}'
-	 }
-	,{
-		name:'between ? and ? lowercase letters',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) lowercase letters',
-		out:'[a-z]{$2,$4}'
-	 }
-	,{
-		name:'any amount of lowercase letters',
-		in:'any( number of| amount of)? lowercase letters',
-		out:'[a-z]+'
-	 }
-	,{
-		name:'a uppercase letter',
-		in:'(a|a single|one|1) uppercase letter',
-		out:'[A-Z]'
-	 }
-	,{
-		name:'? uppercase letters',
-		in:'([0-9]+) uppercase letters',
-		out:'[A-Z]{$1}'
-	 }
-	,{
-		name:'between ? and ? uppercase letters',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) uppercase letters',
-		out:'[A-Z]{$2,$4}'
-	 }
-	,{
-		name:'any amount of uppercase letters',
-		in:'any( number of| amount of)? uppercase letters',
-		out:'[A-Z]+'
-	 }
-	,{
-		name:'any amount of numbers and/or letters',
-		in:'any( number of| amount of)? numbers( &| and| or)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
-		out:'[a-zA-Z0-9]+'
-	 }
-	,{
-		name:'any amount of numbers and/or lowercase letters',
-		in:'any( number of| amount of)? numbers( &| and| or)? lowercase letters',
-		out:'[a-zA-Z0-9]+'
-	 }
-	,{
-		name:'any amount of numbers and/or uppercase letters',
-		in:'any( number of| amount of)? numbers( &| and| or)? uppercase letters',
-		out:'[a-zA-Z0-9]+'
-	 }
-	,{
-		name:'a word',
-		in:'(a|one|1) word',
-		out:'\\w'
-	 }
-	,{
-		name:'? words',
-		in:'([0-9]+) words',
-		out:'\\w{$1}'
-	 }
-	,{
-		name:'between ? and ? words',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) words',
-		out:'\\w{$2,$4}'
-	 }
-	,{
-		name:'a non-word',
-		in:'(a|one|1) non-word',
-		out:'\\W'
-	 }
-	,{
-		name:'? non-words',
-		in:'([0-9]+) non-words',
-		out:'\\W{$1}'
-	 }
-	,{
-		name:'between ? and ? non-words',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) non-words',
-		out:'\\W{$2,$4}'
-	 }
-	,{
-		name:'a digit',
-		in:'(a|one|1) digit',
-		out:'\\d'
-	 }
-	,{
-		name:'? digits',
-		in:'([0-9]+) digits',
-		out:'\\d{$1}'
-	 }
-	,{
-		name:'between ? and ? digits',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) digits',
-		out:'\\d{$2,$4}'
-	 }
-	,{
-		name:'a non-digit',
-		in:'(a|one|1) non-digit',
-		out:'\\D'
-	 }
-	,{
-		name:'? non-digits',
-		in:'([0-9]+) non-digits',
-		out:'\\D{$1}'
-	 }
-	,{
-		name:'between ? and ? non-digits',
-		in:'(between|from) ([0-9]+) (and|to) ([0-9]+) non-digits',
-		out:'\\D{$2,$4}'
-	 }
-]});
+define({
+	list:[
+		 {
+			name:'anything',
+			in:'anything',
+			out:'.*'
+		 }
+		,{
+			name:'start of the line',
+			in:'(from the )?(start|beginning) of (the )?line',
+			out:'^'
+		 }
+		,{
+			name:'end of the line',
+			in:'(to the )?end of (the )?line',
+			out:'$'
+		 }
+		,{
+			name:'new line',
+			in:'(new( |\-)?line|enter)',
+			out:'(\\r)?\\n'
+		 }
+		,{
+			name:'non white space',
+			in:'no(n|t)? white ?space',
+			out:'\\S'
+		 }
+		,{
+			name:'white space',
+			in:'white ?space',
+			out:'\\s'
+		 }
+		,{
+			name:'a number',
+			in:'(a |a single |one |1 )?number',
+			out:'[0-9]'
+		 }
+		,{
+			name:'numbers',
+			in:'(one or more )?numbers',
+			out:'[0-9]+'
+		 }
+		,{
+			name:'any amount of numbers',
+			in:'any( number of| amount of)? numbers',
+			out:'[0-9]+'
+		 }
+		,{
+			name:'? numbers',
+			in:'([0-9]+ ) numbers?',
+			out:'[0-9]{$1}'
+		 }
+		,{
+			name:'between ? and ? numbers',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) numbers',
+			out:'[0-9]{$2,$4}'
+		 }
+		,{
+			name:'a letter',
+			in:'(a|a single|one|1)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letter',
+			out:'[a-zA-Z]'
+		 }
+		,{
+			name:'letters',
+			in:'(one or more )?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z]+'
+		 }
+		,{
+			name:'any amount of letters',
+			in:'any( number of| amount of)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z]+'
+		 }
+		,{
+			name:'? letters',
+			in:'([0-9]+)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z]{$1}'
+		 }
+		,{
+			name:'between ? and ? letters',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+)( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z]{$2,$4}'
+		 }
+		,{
+			name:'a lowercase letter',
+			in:'(a|a single|one|1) lowercase letter',
+			out:'[a-z]'
+		 }
+		,{
+			name:'lowercase letters',
+			in:'(one or more )?lowercase letters',
+			out:'[a-z]+'
+		 }
+		,{
+			name:'any amount of lowercase letters',
+			in:'any( number of| amount of)? lowercase letters',
+			out:'[a-z]+'
+		 }
+		,{
+			name:'? lowercase letters',
+			in:'([0-9]+) lowercase letters',
+			out:'[a-z]{$1}'
+		 }
+		,{
+			name:'between ? and ? lowercase letters',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) lowercase letters',
+			out:'[a-z]{$2,$4}'
+		 }
+		,{
+			name:'an uppercase letter',
+			in:'(a|an|a single|one|1) uppercase letter',
+			out:'[A-Z]'
+		 }
+		,{
+			name:'uppercase letters',
+			in:'(one or more )?uppercase letters',
+			out:'[A-Z]+'
+		 }
+		,{
+			name:'any amount of uppercase letters',
+			in:'any( number of| amount of)? uppercase letters',
+			out:'[A-Z]+'
+		 }
+		,{
+			name:'? uppercase letters',
+			in:'([0-9]+) uppercase letters',
+			out:'[A-Z]{$1}'
+		 }
+		,{
+			name:'between ? and ? uppercase letters',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) uppercase letters',
+			out:'[A-Z]{$2,$4}'
+		 }
+		,{
+			name:'a number and/or letter',
+			in:'(a )?number( &| and| or)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letter',
+			out:'[a-zA-Z0-9]'
+		 }
+		,{
+			name:'numbers and/or letters',
+			in:'(one or more )?numbers( &| and| or)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z0-9]+'
+		 }
+		,{
+			name:'any amount of numbers and/or letters',
+			in:'any( number of| amount of)? numbers( &| and| or)?( uppercase or lowercase| uppercase/lowercase| lowercase or uppercase| lowercase/uppercase)? letters',
+			out:'[a-zA-Z0-9]+'
+		 }
+		,{
+			name:'a number and/or lowercase letter',
+			in:'(a )?number( &| and| or)? lowercase letter',
+			out:'[a-z0-9]'
+		 }
+		,{
+			name:'numbers and/or lowercase letters',
+			in:'(one or more )?numbers( &| and| or)? lowercase letters',
+			out:'[a-z0-9]+'
+		 }
+		,{
+			name:'any amount of numbers and/or lowercase letters',
+			in:'any( number of| amount of)? numbers( &| and| or)? lowercase letters',
+			out:'[a-z0-9]+'
+		 }
+		,{
+			name:'a number and/or uppercase letter',
+			in:'(a )?number( &| and| or)? uppercase letter',
+			out:'[A-Z0-9]'
+		 }
+		,{
+			name:'numbers and/or uppercase letters',
+			in:'(one or more )?numbers( &| and| or)? uppercase letters',
+			out:'[A-Z0-9]+'
+		 }
+		,{
+			name:'any amount of numbers and/or uppercase letters',
+			in:'any( number of| amount of)? numbers( &| and| or)? uppercase letters',
+			out:'[a-zA-Z0-9]+'
+		 }
+		,{
+			name:'a word',
+			in:'(a|one|1) word',
+			out:'\\w'
+		 }
+		,{
+			name:'words',
+			in:'words',
+			out:'\\w+'
+		 }
+		,{
+			name:'? words',
+			in:'([0-9]+) words',
+			out:'\\w{$1}'
+		 }
+		,{
+			name:'between ? and ? words',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) words',
+			out:'\\w{$2,$4}'
+		 }
+		,{
+			name:'a non-word',
+			in:'(a|one|1) non-word',
+			out:'\\W'
+		 }
+		,{
+			name:'non-words',
+			in:'non-words',
+			out:'\\W+'
+		 }
+		,{
+			name:'? non-words',
+			in:'([0-9]+) non-words',
+			out:'\\W{$1}'
+		 }
+		,{
+			name:'between ? and ? non-words',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) non-words',
+			out:'\\W{$2,$4}'
+		 }
+		,{
+			name:'a digit',
+			in:'(a|one|1) digit',
+			out:'\\d'
+		 }
+		,{
+			name:'digits',
+			in:'digits',
+			out:'\\d+'
+		 }
+		,{
+			name:'? digits',
+			in:'([0-9]+) digits',
+			out:'\\d{$1}'
+		 }
+		,{
+			name:'between ? and ? digits',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) digits',
+			out:'\\d{$2,$4}'
+		 }
+		,{
+			name:'a non-digit',
+			in:'(a|one|1) non-digit',
+			out:'\\D'
+		 }
+		,{
+			name:'non-digits',
+			in:'non-digits',
+			out:'\\D+'
+		 }
+		,{
+			name:'? non-digits',
+			in:'([0-9]+) non-digits',
+			out:'\\D{$1}'
+		 }
+		,{
+			name:'between ? and ? non-digits',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) non-digits',
+			out:'\\D{$2,$4}'
+		 }
+	],
+	shortcuts: [
+		 {
+			name:'email', // modified to include lowercase letters from http://www.regular-expressions.info/email.html
+			in:'email',
+			out:'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$'
+		 }
+		,{
+		 	name:'uk postcode', // http://regexlib.com/REDetails.aspx?regexp_id=260
+		 	in:'(uk|UK) post(al)? ?code',
+		 	out:'^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$'
+		 }
+		,{
+			name:'a vowel',
+			in:'(a |one |1 )?vowel',
+			out:'[aeiou]'
+		 }
+		,{
+			name:'vowels',
+			in:'(one or more )?vowels',
+			out:'[aeiou]+'
+		 }
+		,{
+			name:'? vowels',
+			in:'([0-9]+) vowels',
+			out:'[aeiou]{$1}'
+		 }
+		,{
+			name:'between ? and ? vowels',
+			in:'(between|from) ([0-9]+) (and|to) ([0-9]+) vowels',
+			out:'[aeiou]{$2,$4}'
+		 }
+		,{
+			name:'IP address', // http://regexlib.com/REDetails.aspx?regexp_id=32
+			in:'(ip address|IP|^ip$)( address)?',
+			out:'^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$'
+		 }
+		,{
+			name:'Phone Number', // http://regexlib.com/REDetails.aspx?regexp_id=73
+			in:'([tT]ele)?([pP]hone) num(ber)?',
+			out:'^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$'
+		 }
+		,{
+			name:'HTML tag', // http://www.regular-expressions.info/examples.html
+			in:'(HTML|html)( tag)?',
+			out:'<([a-zA-Z][a-zA-Z0-9]*)\b[^>]*>(.*?)</\1>'
+		 }
+		,{
+			name:'Hex colour', // http://stackoverflow.com/questions/1636350/how-to-identify-a-given-string-is-hex-color-format#1636354
+			in:'[hH]ex [cC]olou?r',
+			out:'^#(?:[0-9a-fA-F]{3}){1,2}$'
+		 }
+		,{
+			name:'URL', // http://net.tutsplus.com/tutorials/other/8-regular-expressions-you-should-know/
+			in:'(url|URL)',
+			out:'^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$'
+		 }
+	]
+});
