b42f3e40d1c656f596d3eb7605909562bddab06a
Guy Routledge <guy@bigfish.co.uk>
2013-01-31 21:33:41 +0000

    fix incorrect setting of position on help hide/show

--- scripts/help.js
+++ scripts/help.js
@@ -2,17 +2,18 @@ jQuery(function($){
 
 	$('.help-tab').on('click', function(){
 
-		$('.help').toggleClass('is-active');
 		var helpHeight = $('.help').outerHeight();
 
 		if ( $('.help').is('.is-active') ) {
 			$('.help').animate({
-				'top' : '-=' +  helpHeight
+				'top' : '100%'
 			});
+			$('.help').removeClass('is-active');
 		} else {
 			$('.help').animate({
-				'top' : '+=' +  helpHeight
+				'top' : '-=' +  helpHeight
 			});
+			$('.help').addClass('is-active');
 		}
 		return false;
 
