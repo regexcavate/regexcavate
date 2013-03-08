37eef8e65cc0aa3fffb5b255cd02c224af5d801a
Remy Bach <remy@bach.me.uk>
2013-03-08 17:17:14 +0000

    Add some comments to the config.js file.

--- scripts/locale/en_gb/config.js
+++ scripts/locale/en_gb/config.js
@@ -1,4 +1,6 @@
 window.config = {
+	// A regex used to negate all existing translations.
 	negation: /^(not |no |non )/g,
+	// The text to prepend to the placeholder.
 	placeHolderPrepend: 'Try: '
 };
