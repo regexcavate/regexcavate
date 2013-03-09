4f714f6f37f4b7c00e46eb9c04f953c8f3385a7d
Remy Bach <remy@bach.me.uk>
2013-03-09 00:13:20 +0000

    Update the regex notice message and include improved help tab functionality.

--- scripts/help.js
+++ scripts/help.js
@@ -1,21 +1,22 @@
 jQuery(function($){
 	'use strict';
 
-	$('.help-tab').on('click', function(){
+	$('.help-tab, .help-tab-trigger').on('click', function(){
 
 		var helpHeight = $('.help').outerHeight();
 
 		if ( $('.help').is('.is-active') ) {
 			$('.help').animate({
 				'top' : '100%'
-			});
+			}, focusHelp);
 			$('.help').removeClass('is-active');
 		} else {
 			$('.help').animate({
 				'top' : '-=' +  helpHeight
-			});
+			}, focusHelp);
 			$('.help').addClass('is-active');
 		}
+
 		return false;
 
 	}).on('keydown', function(e){
@@ -24,4 +25,10 @@ jQuery(function($){
 			return false;
 		}
 	});
+
+	function focusHelp() {
+		$('html, body').animate({
+			scrollTop : $('.app').height() - $(window).height()
+		}, 500);
+	}
 });
