597577ad58a4067728fc57ef7e105555e9542feb
Remy Bach <remy@bach.me.uk>
2013-07-17 22:59:44 +0100

    Make tests work within a closure to prevent variable "leaking".

--- tests/spec/en_gb.js
+++ tests/spec/en_gb.js
@@ -1,21 +1,20 @@
 describe("normal", function() {
 	for (tRegex in translationTests) {
-		it(tRegex, function() {
-			for (var i = 0; i < translationTests[tRegex].length; i++) {
-				tPhrase = translationTests[tRegex][i];
-				expect(strToRegex([tPhrase])[0]).toBe(tRegex);
-			}
-		});
+		addTest(translationTests, tRegex);
 	}
 });
 
 describe("shortcuts", function() {
 	for (sRegex in shortcutTests) {
-		it(sRegex, function() {
-			for (var i = 0; i < shortcutTests[sRegex].length; i++) {
-				sPhrase = shortcutTests[sRegex][i];
-				expect(strToRegex([sPhrase])[0]).toBe(sRegex);
-			}
-		});
+		addTest(shortcutTests, sRegex);
 	}
 });
+
+function addTest(testArr, regex) {
+	it(regex, function() {
+		for (var i = 0; i < testArr[regex].length; i++) {
+			phrase = testArr[regex][i];
+			expect(strToRegex([phrase])[0]).toBe(regex);
+		}
+	});
+}
